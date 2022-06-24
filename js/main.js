a3cd6d4866809c264302c80794496f0ff28e5aa3
Paweł <pawel_gnat@o2.pl>
2022-06-24 20:04:42 +0200

    added html css js

--- js/main.js
+++ js/main.js
@@ -1,24 +1,27 @@
 const submitBtn = document.querySelector('.rate-view--submit')
 const rateCard = document.querySelector('.rate-view')
-const btns = document.querySelectorAll('.rate-view__rating-section--rate')
+const rateBtns = document.querySelectorAll('.rate-view__rating-section--rate')
+const resultRate = document.querySelector('.result-view--result-text--rate')
 
-const submitRate = () => {
-	rateCard.classList.add('hidden')
+for (let i = 0; i < rateBtns.length; i++) {
+	rateBtns[i].addEventListener('click', () => {
+		const rate = rateBtns[i].innerHTML
+
+		for (let i = 0; i < rateBtns.length; i++) {
+			rateBtns[i].classList.contains('selected')
+			rateBtns[i].classList.remove('selected')
+		}
+
+		rateBtns[i].classList.add('selected')
+
+		submitBtn.addEventListener('click', e => {
+			if (rateBtns[i].classList.contains('selected')) {
+				e.preventDefault()
+				resultRate.innerHTML = rate
+				rateCard.classList.add('hidden')
+			} else {
+				e.preventDefault()
+			}
+		})
+	})
 }
-
-// selectBtn.forEach(button =>
-// 	button.addEventListener('click', e => {
-// 		console.log('ok')
-// 		selectBtn.classList.add('selected')
-// 	})
-// )
-
-for (consts btn of btns) {
-
-
-	btn.addEventListener('click', function selectBtn () {
-		btn.classList.add('selected');
-	});
-}
-
-submitBtn.addEventListener('click', submitRate)
